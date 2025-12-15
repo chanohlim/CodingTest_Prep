a4220cec7dad64c0e039c360960f0014a6f71abb
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2025-12-15 07:03:21 +0000

    12/15

---
+++ Greedy/ex4_2.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+result = 0
+
+while (n > k):
+
+    target = (n // k) * k # n이 k의 배수가 되는 수로 만들기 => target: n의 초깃값에서 k의 배수가 되는 가장 가까운 수
+    result += (n - target) # result = result + (n - target)
+    n = target
+
+    result += 1
+    n //= k
+
+result += (n - 1)
+print(result)
