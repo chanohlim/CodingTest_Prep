152ea7d707baeeb47a721af4ca96125ff6bd33e4
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2025-12-21 05:57:40 +0000

    .

--- Implementation/ex.py
+++ Implementation/ex.py
@@ -27,6 +27,8 @@ for i in range(n):
 
 directions = [(-1,0), (0,1), (1,0), (0,-1)] # 북, 동, 남, 서 => 캐릭터가 바라보는 방향
 cnt = 1
+sea_cnt = 0
+
 
 # a, b = 현재 좌표
 # da, db = 바라보는 방향의 좌표
@@ -42,13 +44,40 @@ while True:
     da = a + directions[d][0]
     db = b + directions[d][1]
 
-    if da >= n or da < 0 or db >= m or db < 0:
+    print(da, db)
+
+    if da >= n or da < 0 or db >= m or db < 0: # 맵 밖이면 스킵
+        sea_cnt += 1
         continue
 
-    if map_data[da][db] == 0:
-        map_data[a][b] = 1
-        a, b = da, db
-        cnt += 1
+    if map_data[da][db] !=  0: # 바다면 스킵
+        print("바다다!")
+        sea_cnt += 1
+        continue
+
+    if map_data[da][db] == 0: # 바라보는 방향이 가지 않은 육지
+        
+        print("육지다!")
+        map_data[a][b] = 2 # 현재 있는 칸은 방문 표시
+        a, b = da, db # 바라보는 칸으로 이동
+        print(a, b,'로 이동 완료!')
+        cnt += 1 # 이동 횟수 + 1
+
+    
+    if sea_cnt > 3:
+
+        map_data[a][b] = 2
+        da = a + directions[d-2][0]
+        db = b + directions[d-2][1]
+        if map_data[da][db] != 1:
+            a, b = da, db
+        else:
+            break
+
+print(cnt)
+
+print(map_data)
+
 
 
 
