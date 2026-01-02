b33baa32c1a50233924815ba11ca50b0013ac836
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-01-02 15:40:43 +0000

    ,

---
+++ Binary_Search/binary_search_iter.py
@@ -0,0 +1,38 @@
+'''
+
+binary search iteration
+
+'''
+
+arr = [4, 6, 22, 39, 25, 345, 2, 1, 24, 56]
+
+arr.sort()
+
+print(arr)
+
+def binary(array, target):
+
+    start = 0
+    end = len(array) - 1
+    
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        print(start, end, mid, array[mid])
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] > target:
+            end = mid - 1
+
+        else: # arr[mid] < target
+            start = mid + 1
+
+
+    return None
+
+n = int(input())
+
+print(binary(arr,n))
