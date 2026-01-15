e95fbce69242555448dc7d9849dc04025ce59771
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-01-15 14:39:23 +0000

    .

--- Dynamic_Programming/boj_#2579.py
+++ Dynamic_Programming/boj_#2579.py
@@ -74,7 +74,7 @@ for i in range(2, N + 1):
             print('이전 계단 한 칸 올라옴')
 
             choice[i] = 2 # i번째 계단을 오려고 계단 두 개 올라옴
-            dp[i] = dp[i-2] + stairs[i-1]
+            dp[i] = dp[i-2] + stairs[i-1] 
 
     else:
 
