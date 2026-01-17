ad45317765b834c1e2592a5b07b69d1fb819bcb1
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-01-17 14:09:53 +0000

    .

--- Shortest_Path/dijkstra_simple.py
+++ Shortest_Path/dijkstra_simple.py
@@ -21,5 +21,65 @@
    따라서, 마지막 노드는 연결된 간선들을 확인할 필요가 없다.
 
 
+입력:
+6 11
+1
+1 2 2
+1 3 5
+1 4 1
+2 3 3
+2 4 2
+3 2 3
+3 6 5
+4 3 3
+4 5 1
+5 3 1
+5 6 2
+
+
+출력:
+0
+2
+3
+1
+2
+4
+
 '''
 
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+
+# 시작 노드 번호를 입력받기
+start = int(input())
+
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보 입력받기
+for i in range(m):
+    
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b,c))
+
+
+def smallest_node(graph, n):
+
+    smallest = graph[n][0]
+    for node in graph[n]:
+        if node[1] < smallest:
+            smallest = node[0]
+    
+    return smallest
+
+print(graph)
+print(smallest_node(graph, 1))
