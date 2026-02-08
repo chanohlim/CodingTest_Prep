82503b3744e7d8b353f0392e88c08b98ce9a908a
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-02-08 13:53:59 +0000

    .

---
+++ Greedy/Problems/p#04.py
@@ -0,0 +1,52 @@
+'''
+
+#04 만들 수 없는 금액
+
+입력 예시:
+5
+3 2 1 1 9
+
+출력 예시:
+8
+
+'''
+
+N = int(input())
+
+coins = list(map(int, input().split()))
+coins.sort(reverse = True)
+
+print(coins)
+
+for i in range(1, sum(coins)+1):
+
+    idx = 0
+    result = 0
+
+    if min(coins) > i:
+        print(f"not able to make {i}")
+        break
+
+    while coins[idx] > i:
+        idx += 1
+
+    print(f'current:{i}, value[idx]:{coins[idx]},{idx} =>', end = ' ')
+
+    for coin in coins[idx:]:
+        
+        if (result + coin) <= i:
+            result += coin
+            print(coin, end= ' ')
+            if result == i:
+                break
+
+    print('=', result)
+
+    print()
+    
+    if result != i:
+        print(f"{result} - not able to make {i}")
+        break
+
+    
+
