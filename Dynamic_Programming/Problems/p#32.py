2a030b8e901a3008dcb5190d3a300298c9221a96
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-04-04 04:55:35 +0000

    .

--- Dynamic_Programming/Problems/p#32.py
+++ Dynamic_Programming/Problems/p#32.py
@@ -18,4 +18,13 @@ for i in range(n):
 
 for i in range(1, n):
     for j in range(i + 1):
-        
+        if (j == 0):
+            dp[i][j] += dp[i-1][j]
+            continue
+        if (j == i):
+            dp[i][j] += dp[i-1][j-1]
+            continue
+
+        dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+
+print(max(dp[n-1]))
