9e64772f0a248fbcd15e528a640b0073d42e68b5
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-05-09 07:06:35 +0000

    .

--- Greedy/Problems/p#06.py
+++ Greedy/Problems/p#06.py
@@ -2,35 +2,26 @@ import heapq
 
 def solution(food_times, k):
     
-    
-    if k >= sum(food_times):
+    if sum(food_times) <= k:
         return -1
-
-    pq = list()
-
+    
+    pq = []
+    length = len(food_times)
+    
     for i in range(len(food_times)):
         heapq.heappush(pq, (food_times[i], i+1))
-
-    previous = 0
-    length = len(food_times)
-
-    while (pq[0][0] - previous) * length <= k:
-
-        now = heapq.heappop(pq)[0]
-        print(k)
-        k -= (now - previous) * length
-        print(k)
-        length -= 1
-
-        previous = now
         
+    previous = 0
+        
+    while (pq[0][0]-previous) * length <= k:
+        
+        now, index = heapq.heappop(pq)
+        
+        k -= (now-previous) * length
+        length -= 1
+        
+        previous = now
 
-    result = sorted(pq, key = lambda x: x[1])
-
-    return result[k%length][1]
-
-
-food_times = list(map(int, input().split()))
-k = int(input())
-
-print(solution(food_times, k))
+    pq.sort(key = lambda x: x[1])
+    return pq[k % length][1]
+        
