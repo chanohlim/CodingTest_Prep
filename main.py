7bbfae4471e2baf6999a3979b34c43c7575e213c
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-06-15 13:57:39 +0000

    .

--- main.py
+++ main.py
@@ -19,3 +19,40 @@ NO
 
 '''
 
+N = int(input())
+
+graph = []
+
+for i in range(N):
+    graph.append(list(map(int, input().split())))
+
+teachers = []
+answer = False
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 'T':
+            teachers.append((i, j))
+
+
+def radar():
+
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for teacher in teachers:
+        i, j = teacher
+        for k in range(4):
+
+            di, dj = i + direction[k][0], j + direction[k][1]
+            if 
+
+
+def backtracking(a, b, length, N):
+    global answer
+
+    if length == 3:
+        answer = answer or radar()
+
+    for i in range(a, N):
+        for j in range(b, N):
+            graph[i][j] = 'O'
+            backtracking(i)
